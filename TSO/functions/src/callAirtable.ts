4486093bfc2a5654bf37c530b289b98bc4ffd299
funnise <motoifunase@gmail.com>
2019-07-11 08:26:44 +0900

    share

--- TSO/functions/src/callAirtable.ts
+++ TSO/functions/src/callAirtable.ts
@@ -1,15 +1,17 @@
 const Airtable = require('airtable');
-
+export const callAirtable=(data:any)=>{
 var base = new Airtable({apiKey: 'keypaApcLOjHCGgZD'}).base('app1SYNrXjvOclF2i');
     base('Schedule').create({
-      "Event name": "TestEvent2",
+      "Event name": data.name,
       "Date & Time　（JST)": "2019-01-24T19:00:00.000Z",
-      "ref url":"https://google.com",
+      "ref url":data.eventUrl,
+      "Topic/Theme":data.category,
       "Report url ( Confluence)": "https://new-tech.atlassian.net/wiki/spaces/ER/pages/834961421/Test?atlOrigin=eyJpIjoiOTIyMTZjYTY4YzI2NDA2YmExMDY4MDY0N2QzMzBlOTUiLCJwIjoiYyJ9\n",
-
+    
     }, function(err:Error, record:any) {
       if (err) {
         console.error(err);
         return;
       }
     });
+  }
--- TSO/functions/src/index.ts
+++ TSO/functions/src/index.ts
@@ -1,12 +1,10 @@
 import * as functions from 'firebase-functions';
 import { createSlackAttachment } from './createSlackAttachment'
+import { slackToken } from './config'
+import {callAirtable} from './callAirtable'
+import { elements } from './elements'
 const { WebClient } = require('@slack/web-api');
-const token ='xoxb-330894282468-674056438866-bPq7JuTLuFFHvloymXFuqAUG'
-
-const web = new WebClient(token);
-//const Airtable = require('airtable');
-//const request = require('request-promise-native');
-
+const web = new WebClient(slackToken);
 
 exports.airtableRequest = functions.https.onRequest(async(req, res) => {  
     let payload = req.body;
@@ -18,18 +16,20 @@ exports.airtableRequest = functions.https.onRequest(async(req, res) => {
         const option = {
             attachments:[createSlackAttachment('Event情報の入力')],
             channel: payload.event.channel,
-            icon_emoji: ':question:',
+            icon_emoji: ':jouhouhassinn:',
             username: 'Event Infomation Manager'
           }
           web.chat.postMessage(option)
           .then((response:any) => {
              console.log('メッセージを送信しました: ', response.ts)
-          }).catch(console.error)     
+          }).catch((err:any)=>{
+              console.log(err)
+              console.log('エラー発生')
+          })     
     　 }
     if (typeof payload.payload === 'string') {
         payload = JSON.parse(payload.payload)
     }
-    
     switch (payload.type) {
         case 'interactive_message':
         web.dialog.open({
@@ -38,54 +38,18 @@ exports.airtableRequest = functions.https.onRequest(async(req, res) => {
               title: "イベント情報",
               submit_label: "送信する",
               notify_on_cancel: true,
-              elements: [
-                {
-                  type: "text",
-                  label: "event name",
-                  name: "name",
-                  placeholder: 'foo'
-                },
-                {
-                  type: "text",
-                  label: "event url",
-                  name: "url",
-                  placeholder: "https://google.com",
-                },
-                {
-                  label: "Tech category",
-                  type: "select",
-                  name: "review",
-                  options: [
-                    {
-                      label: "AI",
-                      value: "AI"
-                    },
-                    {
-                      label: "Tool",
-                      value: "Tool"
-                    },
-                    {
-                      label: "Method",
-                      value: "Method"
-                    },
-                    {
-                      label: "Other",
-                      value: "Other"
-                    },
-                  ]
-                },
-              ]
+              elements: elements
             },
             trigger_id: payload.trigger_id
           })
             .then(() => {
               console.log('ダイアログを送信しました')
-            })
-            .catch(console.error)
-          
+            }).catch(console.error)
             break
         case 'dialog_submission':
-                console.log('ダイアログの結果を受信しました')
+              console.log('ダイアログの結果を受信しました')
+              console.log(payload.submission)
+              callAirtable(payload.submission)
             break
     }
      return res.status(200).send('')
